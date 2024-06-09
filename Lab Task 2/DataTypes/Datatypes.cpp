122872efb0f55604c691fec8c56e97b80315a712
BSC-ELE-30-22 <157712141+HendrickGondwe@users.noreply.github.com>
2024-06-09 23:08:15 +0100

    Data types.cpp

--- Lab Task 2/DataTypes/Datatypes.cpp
+++ Lab Task 2/DataTypes/Datatypes.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Size of char : " << sizeof(char) << endl;
-    cout << "Size of int : " << sizeof(int) << endl;
-    cout << "size of short int : " << sizeof(short int) << endl;
-    cout << "size of long int : " << sizeof(long int)<< endl;
-    cout << "size of float : " << sizeof(float) << endl;
-    cout << "size of double : " << sizeof(double) << endl;
-    cout << "size of wchar_t : " << sizeof(wchar_t) << endl;
+        cout << "Size of char : " << sizeof(char) << endl;
+        cout << "Size of int : " << sizeof(int) << endl;
+        cout << "size of short int : " << sizeof(short int) << endl;
+        cout << "size of long int : " << sizeof(long int)<< endl;
+        cout << "size of float : " << sizeof(float) << endl;
+        cout << "size of double : " << sizeof(double) << endl;
+        cout << "size of wchar_t : " << sizeof(wchar_t) << endl;
 
-    return 0;
+ return 0;
 }
