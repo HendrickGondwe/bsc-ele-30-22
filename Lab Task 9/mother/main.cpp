5fe7b3ac75d5b0b53a24662207a95834d9336cf5
BSC-ELE-30-22 <157712141+HendrickGondwe@users.noreply.github.com>
2024-06-10 01:07:28 +0200

    Main.cpp

--- Lab Task 9/mother/main.cpp
+++ Lab Task 9/mother/main.cpp
@@ -3,9 +3,8 @@
 #include "Son.h"
 using namespace std;
 
-int main() {
-    Daughter theDaughter(1);
-    Son theSon(2);
-    
+    int main() {
+        Daughter theDaughter(1);
+        Son theSon(2);   
     return 0;
 }
