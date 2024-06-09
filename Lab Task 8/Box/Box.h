f8ae8e1144a51800315d181c96d0705b9223f5e6
BSC-ELE-30-22 <157712141+HendrickGondwe@users.noreply.github.com>
2024-06-10 01:12:43 +0200

    Box.h

--- Lab Task 8/Box/Box.h
+++ Lab Task 8/Box/Box.h
@@ -1,26 +1,23 @@
 #pragma once
-
 #include <iostream>
 using namespace std;
 
-class Box
+    class Box
 {
-public:
-    Box();
-    Box(const double newLength, const double newBreadth, const double newHeight);
+    public:
+        Box();
+        Box(const double newLength, const double newBreadth, const double newHeight);
 
-    ~Box();
+        ~Box();
 
-    double GetVolume();
+        double GetVolume();
+    
+        void SetLength(double len);
+        void SetBreadth(double bre);
+        void SetHeight(double hei);
 
-    void SetLength(double len);
-
-    void SetBreadth(double bre);
-
-    void SetHeight(double hei);
-
-    Box operator + (const Box&);
-
+        Box operator + (const Box&);
+    
     private:
 
         double length;
