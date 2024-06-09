1635d0719ec3bec05eb11a12c12ffc5305aee1df
BSC-ELE-30-22 <157712141+HendrickGondwe@users.noreply.github.com>
2024-06-10 01:11:11 +0200

    Box.cpp

--- Lab Task 8/Box/Box.cpp
+++ Lab Task 8/Box/Box.cpp
@@ -1,44 +1,40 @@
 #pragma once
-
 #include "Box.h"
-
-Box::Box()
+ Box::Box()
 {
     length = 0.0;
     breadth = 0.0;
     height =0.0 ;
 }
-
-Box::Box(const double  newLength, const double newBreadth, const double newHeight){
+    Box::Box(const double  newLength, const double newBreadth, const double newHeight){
     length = newLength;
     breadth = newBreadth;
     height = newHeight;
 }
-Box ::~Box(){
+ Box ::~Box(){
 
 }
-double Box::GetVolume(){
-    return length * breadth * height;
+    double Box::GetVolume(){
+       return length * breadth * height;
 }
 
-void Box::SetLength(double len){
-    length = len;
+    void Box::SetLength(double len){
+        length = len;
 }
 
-void Box::SetBreadth(double bre){
-    breadth = bre;
+    void Box::SetBreadth(double bre){
+        breadth = bre;
 }
 
-void Box::SetHeight(double hei){
-    height = hei;
+    void Box::SetHeight(double hei){
+        height = hei;
 }
 
-//Overload + operator to add two objects.
 
 Box Box::operator+(const Box& b){
     Box box;
     box.length = this->length + b.length;
     box.breadth = this->breadth + b.breadth;
     box.height = this->height + b.height;
-    return box;
+ return box;
 }
