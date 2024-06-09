932429bf686489070333c99ed0a8d0a93c67e798
BSC-ELE-30-22 <157712141+HendrickGondwe@users.noreply.github.com>
2024-06-10 00:49:27 +0200

    Main.cpp

--- Lab Task 9/Polygon/main.cpp
+++ Lab Task 9/Polygon/main.cpp
@@ -2,18 +2,17 @@
 #include "Rectangle.h"
 #include "Triangle.h"
 using namespace std;
-
-int main() {
-    Rectangle rect;
-    Triangle trgl;
-    Polygon* pPoly1 = &rect;
-    Polygon* pPoly2 = &trgl;
+    int main() {
+        Rectangle rect;
+        Triangle trgl;
+        Polygon* pPoly1 = &rect;
+        Polygon* pPoly2 = &trgl;
     
-    pPoly1->SetValues(4, 5);
-    pPoly2->SetValues(4, 5);
+        pPoly1->SetValues(4, 5);
+        pPoly2->SetValues(4, 5);
     
-    pPoly1->PrintArea();
-    pPoly2->PrintArea();
+        pPoly1->PrintArea();
+        pPoly2->PrintArea();
     
     return 0;
 }
