4bb9b67bc7566017c895544afb5f77de636496fc
BSC-ELE-30-22 <157712141+HendrickGondwe@users.noreply.github.com>
2024-06-10 01:18:30 +0200

    Rectangle Tester.cpp

--- Lab Task 7/Rectangle overloaded constractor/RectangleTester.cpp
+++ Lab Task 7/Rectangle overloaded constractor/RectangleTester.cpp
@@ -2,31 +2,28 @@
 #include "Rectangle.h"
 
 using namespace std;
-
-int main() {
+    int main() {
     
-    Rectangle rectangle1;
+        Rectangle rectangle1;
 
-    float length, width;
+        float length, width;
 
-    cout << "Enter length of the rectangle: ";
-    cin >> length;
-    cout << "Enter width of the rectangle: ";
-    cin >> width;
+        cout << "Enter length of the rectangle: ";
+        cin >> length;
+        cout << "Enter width of the rectangle: ";
+        cin >> width;
 
     rectangle1.setLength(length);
     rectangle1.setWidth(width);
 
-    cout << "Area of the first rectangle: " << rectangle1.calculateArea() << endl;
-
-    cout << "\nEnter length of the second rectangle: ";
-    cin >> length;
-    cout << "Enter width of the second rectangle: ";
-    cin >> width;
+        cout << "Area of the first rectangle: " << rectangle1.calculateArea() << endl;
+       cout << "\nEnter length of the second rectangle: ";
+        cin >> length;
+        cout << "Enter width of the second rectangle: ";
+        cin >> width;
 
     Rectangle rectangle2(length, width);
-
-    cout << "Area of the second rectangle: " << rectangle2.calculateArea() << endl;
+          cout << "Area of the second rectangle: " << rectangle2.calculateArea() << endl;
 
     return 0;
 }
