d1e3be0828f44e0612f0d6231378c02d63cc6698
BSC-ELE-30-22 <157712141+HendrickGondwe@users.noreply.github.com>
2024-06-09 23:24:31 +0100

    Conditional Operator.cpp

--- Lab Task 3/ConditionalOperator.cpp
+++ Lab Task 3/ConditionalOperator.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main()
-{
+    {
     int x , y = 10;
 
-    x = (y < 10) ? 30 : 40;
-    cout << "value of x :" << x << endl;
+        x = (y < 10) ? 30 : 40;
+        cout << "value of x :" << x << endl;
 
     return 0;
     
-}
+    }
