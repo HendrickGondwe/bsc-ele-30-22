206c1e0428f990c6cd23b234d72ad6e2113a011a
BSC-ELE-30-22 <157712141+HendrickGondwe@users.noreply.github.com>
2024-06-10 05:34:09 +0200

    While Loop.cpp

--- Lab Task 5/WhileLoop.cpp
+++ Lab Task 5/WhileLoop.cpp
@@ -11,9 +11,7 @@ int main()
 
     while (cin.fail() || valueNumber < 5 || valueNumber > 10) 
     {
-        
         cin.clear();
-        
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         
         cout << "Invalid input. Please enter an integer value between 5 and 10: ";
